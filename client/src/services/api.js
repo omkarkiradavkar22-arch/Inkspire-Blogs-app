9db12808e34e3a78b98db7ba8798501aa1a51c16
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-06-28 06:31:25 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://inkspire-blogs-app1.onrender.com/api/blogs",
-  // baseURL: "http://localhost:5000/api/blogs",
 });
 
 API.interceptors.request.use((req) => {
