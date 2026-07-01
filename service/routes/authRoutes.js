028843bcc4871889a698e966909e444575307f8a
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-01 09:45:20 +0530

    Update authRoutes.js

--- service/routes/authRoutes.js
+++ service/routes/authRoutes.js
@@ -43,7 +43,7 @@ router.post("/forgot-password", async (req, res) => {
   }
 });
 
-    const resetLink = `inkspire-blogs-app.vercel.app/${resetToken}`;
+    const resetLink = `https://inkspire-blogs-app.vercel.app/${resetToken}`;
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
