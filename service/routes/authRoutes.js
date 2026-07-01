e127337ba29240b335bd9dd67867522a29876f08
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-01 10:32:30 +0530

    Update authRoutes.js

--- service/routes/authRoutes.js
+++ service/routes/authRoutes.js
@@ -32,7 +32,9 @@ router.post("/forgot-password", async (req, res) => {
     await user.save();
 
     const transporter = nodemailer.createTransport({
-  service: "gmail",
+   host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // 587 = false
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
